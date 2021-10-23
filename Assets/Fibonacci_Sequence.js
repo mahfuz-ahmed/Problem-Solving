2c7fbdc6f7fc79f0fb173f55dd215f2633475ee2
Mahfuz <mahfuzkst2@gmail.com>
2021-10-23 16:20:29 +0600

    add user input in  fibonacci-Sequance

--- Assets/Fibonacci_Sequence.js
+++ Assets/Fibonacci_Sequence.js
@@ -4,9 +4,25 @@ a = 0; //0,1,1,2
 b = 1; //1,1,2,3
 result = b; //1,1,2,3
 
-for (x = 0; x < 10; x++) {
-  document.write(result + "<br/>"); //1,1,2,3
+for (x = 1; x <= 10; x++) {
+  console.log(result); //1,1,2,3
   result = a + b; //0+1=1,1+1=2,1+2=3,2+3=5,
   a = b; //1,1,2,3
   b = result; //1,2,3,5
+}
+
+// User Input
+
+const number = parseInt(prompt("Enter a positive number: "));
+
+let n1, n2, sum;
+n1 = 0;
+n2 = 1;
+sum = n2;
+
+for (x = 1; x <= number; x++) {
+  document.write(sum + "<br/>");
+  sum = n1 + n2;
+  n1 = n2;
+  n2 = sum;
 }
